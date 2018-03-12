ab890e65d69b5bcc82ca924b4352e6dd62d7f22e
cd09 <34310947+cd09@users.noreply.github.com>
2018-03-11 20:36:04 -0400

    Update WebService.cs

--- WebService.cs
+++ WebService.cs
@@ -52,7 +52,7 @@ namespace WcfLeadService
             try
             {
                 myLog.AppendLine("Parameters retrieved: ");
-                myLog.AppendLine("LeadId: " + LeadId.ToString());
+                myLog.AppendLine("Id: " + Id.ToString());
                 myLog.AppendLine("Type: " + Type.ToString());
                 myLog.AppendLine("LeadDate: " + LeadDate);
                 myLog.AppendLine("AdvisorName: " + AdvisorName);
@@ -70,7 +70,7 @@ namespace WcfLeadService
                     Results refInfo = new Results
                     {
                         Result = myLog.ToString(),
-                        ErrorMessage = "Can't Authenticate to CRM. Please verify that the credentials are valid. Message: " + e.Message
+                        ErrorMessage = "Can't Authenticate. Please validate the credentials. Message: " + e.Message
                     };
 
                     string jsonResult = JsonConvert.SerializeObject(refInfo);
@@ -88,7 +88,7 @@ namespace WcfLeadService
                                 <attribute name='createdby' />
                                 <attribute name='lastname' />
                                     <filter type='and'>
-                                      <condition attribute='contactid' operator='eq' value='" + LeadId + @"' />
+                                      <condition attribute='contactid' operator='eq' value='" + Id + @"' />
                                     </filter>                                    
                               </entity>
                             </fetch>";
@@ -110,7 +110,7 @@ namespace WcfLeadService
                 if (result.Entities.Count > 0)
                 {
                     myLog.AppendLine("Lead already exists");
-                    #region Send Reject Dupe to 3rd party Lead service
+                    #region Send Reject Dupe to 3rd party service
                     myLog.AppendLine("Send Reject Dupe to 3rd party Lead service.");
 
                     var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://www.somewebservice.com/api/RejectDupeLead");
@@ -120,8 +120,8 @@ namespace WcfLeadService
 
                     using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                     {
-                        string json = "[{\"LeadId\":\"" + LeadId + "\"," +
-                                      "\"OrganizationId\":\"" + organizationID + "\"}]";
+                        string json = "[{\"Id\":\"" + Id + "\"," +
+                                      "\"Organization\":\"" + organization + "\"}]";
 
                         streamWriter.Write(json);
                         streamWriter.Flush();
@@ -147,8 +147,8 @@ namespace WcfLeadService
                     httpWebRequest.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(authInfo)));
                     using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                     {
-                        string json = "[{\"LeadId\":\"" + LeadId + "\"," +
-                            "\"OrganizationId\":\"" + organizationID + "\"}]";
+                        string json = "[{\"Id\":\"" + Id + "\"," +
+                            "\"Organization\":\"" + organization + "\"}]";
 
                         streamWriter.Write(json);
                         streamWriter.Flush();
@@ -174,45 +174,30 @@ namespace WcfLeadService
                             }
                             if (returnedErrorDetails != String.Empty)
                             {
-                                ErrorMessage += "Error received from 3rd party Lead service. ";
+                                ErrorMessage += "Error received from 3rd party service. ";
                             }
                         }
 
                         if (returnedErrorDetails == String.Empty)
                         {
-                            if (firstRootObject.Lead.Type != 1) //!= new prospect
-                            {
-                                ErrorMessage += "Expects Type of 1.  Type sent: " + firstRootObject.Lead.Type.ToString();
-                            }
-                            else
-                            {
-                                //create records in crm system
-                            }
+                           //create records in crm system
                         }
                     }
                     #endregion
                 }
 
-                #region Log any errors for customer
+                #region Log any errors
                 if (returnedRejectDupeResult != String.Empty || returnedErrorDetails != String.Empty || ErrorMessage != String.Empty)
                 {
-                    //Log errors for customer
                     Entity note = new Entity(Annotation.EntityLogicalName);
+                    note["subject"] = "Error(s) for Id: " + Id.ToString();
+                    note["notetext"] = "Detail Logs: " + myLog.ToString();
                     if (returnedErrorDetails != String.Empty)
-                    {
-                        note["subject"] = "3rd Party Accept result Error for LeadId: " + LeadId.ToString();
-                        note["notetext"] = "Detail Logs: " + myLog.ToString() + "\r\n 3rd Party Accept Error: " + returnedErrorDetails;
-                    }
-                    else if (returnedRejectDupeResult != String.Empty)
-                    {
-                        note["subject"] = "3rd Party RejectDuplicate result for LeadId: " + LeadId.ToString();
-                        note["notetext"] = "Detail Logs: " + myLog.ToString() + "\r\n RejectDuplicate Result: " + returnedRejectDupeResult;
-                    }
-                    else //error returned from this service/crm
-                    {
-                        note["subject"] = "Targeted LeadInfo Error for LeadId: " + LeadId.ToString();
-                        note["notetext"] = "Detail Logs: " + myLog.ToString() + "\r\n Error: " + Error;
-                    }
+                        note["notetext"] += "\r\n Accept Error: " + returnedErrorDetails;
+                    if (returnedRejectDupeResult != String.Empty)
+                        note["notetext"] += "\r\n RejectDuplicate Result: " + returnedRejectDupeResult;
+                    if (Error != String.Empty)
+                        note["notetext"] += "\r\n Error: " + Error;
                     service.Create(note);
                     myLog.AppendLine("Note log created in CRM.");
                 }
@@ -224,8 +209,8 @@ namespace WcfLeadService
                     Result = myLog.ToString(),
                     Error = ErrorMessage
                 };
-                string json4 = JsonConvert.SerializeObject(refInfoResults);
-                return json4;
+                string json = JsonConvert.SerializeObject(refInfoResults);
+                return json;
             }
             catch (Exception e)
             {
@@ -235,9 +220,9 @@ namespace WcfLeadService
                     Error = "Message: " + e.Message + "Error: " + ErrorMessage
                 };
 
-                //Log service errors for customer
+                //Log service errors
                 Entity note = new Entity(Annotation.EntityLogicalName);
-                note["subject"] = "Targeted LeadInfo Error for LeadId: " + LeadId.ToString();
+                note["subject"] = "Targeted Info Error for Id: " + Id.ToString();
                 note["notetext"] = "Detail Logs: " + myLog.ToString() + "\r\n Message: " + e.Message + "\r\n Error: " + ErrorMessage + "returnedAcceptResult: " + returnedAcceptResult;
                 service.Create(note);
 
@@ -266,7 +251,7 @@ namespace WcfLeadService
 
         public class Lead
         {
-            public int LeadId { get; set; }
+            public int Id { get; set; }
             public byte Type { get; set; }
             public string LeadDate { get; set; }
             public string AdvisorName { get; set; }
